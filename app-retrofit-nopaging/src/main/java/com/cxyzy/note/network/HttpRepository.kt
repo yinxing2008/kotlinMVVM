9f33a69444a159c606355d98fb719b95ddcaaae6
jerry <309032663@qq.com>
2019-04-16 14:48:16 +0800

    update

--- app-retrofit-nopaging/src/main/java/com/cxyzy/note/network/HttpRepository.kt
+++ app-retrofit-nopaging/src/main/java/com/cxyzy/note/network/HttpRepository.kt
@@ -3,5 +3,5 @@ package com.cxyzy.note.network
 import com.cxyzy.note.utils.PAGE_SIZE
 
 object HttpRepository : BaseHttpRepository() {
-    suspend fun getTask() = api.repos(user = "cxyzy1", perPage = 1, page = PAGE_SIZE).await()
+    suspend fun getTask() = api.repos(user = "cxyzy1", page = 1, perPage = PAGE_SIZE).await()
 }
